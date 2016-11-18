5c1bf5ac471e8430c397f1c1efdaa333f0f74fe0
Dana Kulp <dkulp23@gmail.com>
2016-11-17 23:19:36 -0800

    Added jQuery script to render projects to DOM.

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,34 @@
 'use strict';
+
+var projects = [ ];
+
+function Project(obj) {
+  this.title = obj.title;
+  this.completedOn = obj.completedOn;
+  this.numFiles = obj.numFiles;
+  this.ghPages = obj.ghPages;
+  this.imageURL = obj.imageURL;
+  this.imgAlt = obj.imgAlt;
+  this.description = obj.description;
+}
+
+Project.prototype.renderProject = function() {
+  var $project = $('article.template').clone();
+  $project.find('.title').text(this.title);
+  $project.find('.dateComplete').text(this.completedOn);
+  $project.find('.files').text(this.numFiles);
+  $project.find('a').attr('href', this.ghPages);
+  $project.find('img').attr('src', this.imageURL);
+  $project.find('img').attr('alt', this.imgAlt);
+  $project.find('.project-body').html(this.description);
+  $project.removeClass('template');
+  return $project;
+};
+
+myProjects.forEach(function(eachProjectProperty) {
+  projects.push(new Project(eachProjectProperty));
+});
+
+projects.forEach(function(projectInstance){
+  $('#projects').append(projectInstance.renderProject());
+});
