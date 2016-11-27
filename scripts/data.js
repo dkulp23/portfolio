823e29f4e383ef6a57e7b2ff1637f7cb3c98c1db
Dana Kulp <dkulp23@gmail.com>
2016-11-26 18:05:17 -0800

    Added About Me section to HTML and a type property to data objects for filtering.

--- scripts/data.js
+++ scripts/data.js
@@ -6,6 +6,7 @@ var myProjects = [
     ghPages: 'https://dkulp23.github.io/campfire_coffee/',
     imageURL: '#',
     imgAlt: '',
+    type: 'business',
     description: '<p>Campfire Coffee was our introduction to the use of the JavaScript constructor function...</p>',
   },
   {
@@ -15,6 +16,7 @@ var myProjects = [
     ghPages: 'https://dkulp23.github.io/guessing_game/',
     imageURL: '#',
     imgAlt: '',
+    type: 'game',
     description: '<p>Guessing Game was our very first 201 project...</p>',
   },
   {
@@ -24,6 +26,7 @@ var myProjects = [
     ghPages: 'https://dkulp23.github.io/bus-mall/',
     imageURL: '#',
     imgAlt: '',
+    type: 'game',
     description: '<p>Bus Mall was a fun project that allowed us to practice object oriented programming...</p>',
   },
   {
@@ -33,6 +36,7 @@ var myProjects = [
     ghPages: 'https://dkulp23.github.io/sos/',
     imageURL: '#',
     imgAlt: '',
+    type: 'services',
     description: '<p>SOS is a project that my team and I were very proud of...</p>',
   }
 ];
