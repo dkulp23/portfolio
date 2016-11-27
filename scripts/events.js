52fd9aaf1cf63ff1e85f5769587b7d82586516e2
Dana Kulp <dkulp23@gmail.com>
2016-11-26 22:53:14 -0800

    Testing script for navigation button clicks.

--- scripts/events.js
+++ scripts/events.js
@@ -5,6 +5,9 @@ var proNavsAndFilters = { };
 proNavsAndFilters.mainNavListener = function() {
 	$('.main_nav').on('click', '.nav_link', function(){
 		$('.page-section').hide();
-		$(`#${(this).data('nav')}`).fadeIn(1000);
+		$(`#${$(this).data('nav')}`).fadeIn(1000);
 	});
+	$('.main_nav .nav_link:first').click();
 }
+
+proNavsAndFilters.mainNavListener();
