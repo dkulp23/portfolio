91d43fda3a9b549d6b1c542687c38dcc750d5d76
Dana Kulp <dkulp23@gmail.com>
2016-12-03 23:10:58 -0800

    Created methods for the Project object that would use AJAX to check local storage or use AJAX to get data objects for Project constructor.

--- scripts/index.js
+++ scripts/index.js
@@ -13,18 +13,40 @@ function Project(obj) {
   this.description = obj.description;
 }
 
-Project.prototype.renderProject = function() {
+Project.prototype.compileTemplate = function() {
   var blackMagicCompile = Handlebars.compile($('#projectTemplate').html());
   return blackMagicCompile(this);
 };
 
-myProjects.forEach(function(eachProjectProperty) {
-  projects.push(new Project(eachProjectProperty));
-});
+Project.useDataToMakeObjects = function(data) {
+  data.forEach(function(thisProject) {
+    projects.push(new Project(thisProject));
+  })
+};
 
-projects.forEach(function(projectInstance){
-  $('#projects').append(projectInstance.renderProject());
-  if($('#type-filter option:contains("' + projectInstance.projectType + '")').length === 0) {
-    $('#type-filter').append('<option>' + projectInstance.projectType + '</option>');
+Project.getData = function() {
+  if(localStorage.myProjectData) {
+    var localStorageProjects = JSON.parse(localStorage.getItem('myProjectData'));
+    Project.useDataToMakeObjects(localStorageProjects);
+    Project.renderProjects();
+  } else {
+    $.getJSON('/scripts/data.json')
+    .done(function(projectData) {
+      console.log(projectData);
+      Project.useDataToMakeObjects(projectData);
+      Project.renderProjects();
+      localStorage.setItem('myProjectData', JSON.stringify(projectData));
+    })
   }
-});
+};
+
+Project.renderProjects = function() {
+  projects.forEach(function(projectInstance){
+    $('#projects').append(projectInstance.compileTemplate());
+    if($('#type-filter option:contains("' + projectInstance.projectType + '")').length === 0) {
+      $('#type-filter').append('<option>' + projectInstance.projectType + '</option>');
+    }
+  })
+};
+
+Project.getData();
