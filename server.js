f57cb1821ee5e88672ac71f79980e054c821b064
Dana Kulp <dkulp23@gmail.com>
2016-12-29 19:24:28 -0800

    Wrote server code including proxy to handle github api authentication using token set to the environment object.

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var requestProxy = require('express-request-proxy');
+var express = require('express');
+var port = process.env.port || 3000;
+var app = express();
+
+var proxyGHrequest = function(request, response) {
+  console.log(`Routing request for, ${request.params[0]}`);
+  (requestProxy({
+    url: `https://api.github.com/${request.params[0]}`,
+    headers: {
+      Authorization: `token ${process.env.GITHUB_TOKEN}`
+    }
+  }))(request, response)
+};
+
+app.get('/github/*', proxyGHrequest);
+
+app.use(express.static('./'));
+
+app.get('*', function(request, response) {
+  console.log(`New request ${request.url}`);
+  response.sendFile('index.html', {root: '.'});
+});
+
+app.listen(port, function(){
+  console.log(`Listening on port ${port}!`);
+});
